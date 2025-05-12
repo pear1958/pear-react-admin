360d19732a3c22890dbcf2870d352d8cf2a6f5f2
zhangzy <zhangzhiyong@klicen.com>
2025-05-12 17:10:44 +0800

    feat: add dependency

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,5 +1,55 @@
+import { useEffect, useState } from 'react'
+import { Button } from 'antd'
+import { produce } from 'immer'
+import { useImmer } from 'use-immer'
+
 const Home = () => {
-  return <div>Home</div>
+  const [obj, setObj] = useState({
+    name: 'zhangsan',
+    nest: {
+      aaa: {
+        bbb: [21],
+        ccc: 278
+      }
+    }
+  })
+
+  const [data, setData] = useImmer<Recordable>({
+    name: 'lisi',
+    nest: {
+      xxx: {
+        yyy: [77],
+        zzz: 629
+      }
+    }
+  })
+
+  useEffect(() => {
+    console.log('修改了obj', obj)
+  }, [obj])
+
+  useEffect(() => {
+    console.log('修改了data', data)
+  }, [data])
+
+  const handleClick = () => {
+    setObj(
+      produce(obj, draft => {
+        draft.nest.aaa.ccc = 333
+      })
+    )
+    setData(draft => {
+      draft.nest.xxx.zzz = 'oooppppp'
+    })
+  }
+
+  return (
+    <div>
+      <Button onClick={handleClick} type="primary">
+        测试
+      </Button>
+    </div>
+  )
 }
 
 export default Home
