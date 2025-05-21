f788f98667986a8a81825b346ea746fe24b6cc44
zhangzy <zhangzhiyong@klicen.com>
2025-05-21 10:35:25 +0800

    refactor: 优化代码

--- src/layout/components/Main/MemoScroll.tsx
+++ src/layout/components/Main/MemoScroll.tsx
@@ -1,19 +1,19 @@
 import { useEffect, useMemo, useRef } from 'react'
 import { useLocation } from 'react-router-dom'
 
-function MemoScroll({ children, className }) {
-  const domRef = useRef<HTMLDivElement>(null)
-  const location = useLocation()
-  const scrollHistoryMap = useRef<Map<string, number>>(new Map())
+// 当切换路径时, 记住页面的滚动位置
+const MemoScroll = ({ children, className }) => {
+  const { pathname, search } = useLocation()
 
-  const activeKey = useMemo(() => {
-    return location.pathname + location.search
-  }, [location.pathname, location.search])
+  const domRef = useRef<HTMLDivElement>(null)
+  const scrollHistoryMap = useRef<Map<string, number>>(new Map())
+  const activeKey = useMemo(() => pathname + search, [pathname, search])
 
   useEffect(() => {
     const divDom = domRef.current
     if (!divDom) return
 
+    // 200毫秒等待动画过渡效果结束
     setTimeout(() => {
       divDom.scrollTo(0, scrollHistoryMap.current.get(activeKey) || 0)
     }, 200)
@@ -21,6 +21,7 @@ function MemoScroll({ children, className }) {
     const onScroll = (e: Event) => {
       const target = e.target as HTMLDivElement
       if (!target) return
+      // 记录滚动位置
       scrollHistoryMap.current.set(activeKey, target?.scrollTop || 0)
     }
 
