85a5501152b145d7a3a67d15c77ac5d3e2f791ea
zhangzy <zhangzhiyong@klicen.com>
2025-05-28 10:20:07 +0800

    perf: 完善css

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,6 @@ export default defineConfig({
       less: {
         additionalData: `
             @import "@/styles/mixin.less";
-            @import "@/styles/variable.less";
           `
       }
     }
