1ab0d9bac8ff77a4ea841807e7b86c6c9cec933e
zhangzy <doxoops@163.com>
2025-06-03 22:11:04 +0800

    fix: ts

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ import 'virtual:svg-icons-register'
 
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import { persistor, store } from './redux'
+import store, { persistor } from './redux'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -14,7 +14,7 @@ import storage from 'redux-persist/lib/storage'
 import deviceReducer from './modules/device'
 import { isDev } from '@/utils'
 
-export const store = configureStore({
+const store = configureStore({
   reducer: persistReducer(
     {
       key: 'pear-redux',
@@ -35,11 +35,13 @@ export const store = configureStore({
 
 export const persistor = persistStore(store)
 
-export type Thunk<ReturnType = void> = ThunkAction<ReturnType, State, unknown, UnknownAction>
-
 export type State = ReturnType<typeof store.getState>
 export type Dispatch = typeof store.dispatch
+
+export type Thunk<ReturnType = void> = ThunkAction<ReturnType, State, unknown, UnknownAction>
 
 // 创建有类型提示的方法
 export const useSelector: TypedUseSelectorHook<State> = useReduxSelector
 export const useDispatch = () => useReduxDispatch<Dispatch>()
+
+export default store
