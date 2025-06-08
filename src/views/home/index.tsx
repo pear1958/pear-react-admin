92ee50286d596174dd049b5c0bd81c577d91e75f
zhangzy <doxoops@163.com>
2025-06-08 12:38:27 +0800

    fix: 优化代码

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,47 +1,13 @@
-// import { useNavigate } from 'react-router-dom'
-import { Button, Input } from 'antd'
-import useAuthButton from '@/hooks/useAuthButton'
-
 const Home = () => {
-  // const navigate = useNavigate()
-  const { BUTTONS } = useAuthButton()
-
-  const url = '/components/jsonForm'
-
-  console.log('aaaaaaa')
-
-  const handleClick = () => {
-    // navigate(url)
-  }
-
-  const handleClick2 = () => {
-    window.$navigate(url)
-  }
-
   const data = Array(120)
     .fill('')
     .map(_ => Math.random())
 
   return (
     <div className="pear-box">
-      <div>
-        <Button onClick={handleClick} type="primary">
-          测试1
-        </Button>
-        <Button onClick={handleClick2} type="primary" className="ml-3">
-          测试2
-        </Button>
-      </div>
-
-      <div className="mt-3 w-[300px]">
-        <Input placeholder="请输入" />
-      </div>
-
-      <div className="mt-3">
-        {data.map(_ => (
-          <div key={_}>{_}</div>
-        ))}
-      </div>
+      {data.map(_ => (
+        <div key={_}>{_}</div>
+      ))}
     </div>
   )
 }
--- src/views/menu/menu1/index.tsx
+++ src/views/menu/menu1/index.tsx
@@ -1,11 +1,16 @@
-import { Card } from "antd";
+import { useSearchParams } from 'react-router-dom'
+import { Card } from 'antd'
 
 const Menu1: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams()
+  const name = searchParams.get('name')
+  const xxx = searchParams.get('xxx') // null
+
   return (
     <Card>
       <span className="text">我是 Menu 1 页面</span>
     </Card>
-  );
-};
+  )
+}
 
-export default Menu1;
+export default Menu1
