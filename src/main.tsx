6200d88008c06453d8c588855761201e0665636c
zhangzy <doxoops@163.com>
2025-09-05 21:21:04 +0800

    feat: code

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,17 @@ import 'virtual:svg-icons-register'
 import store, { persistor } from './redux'
 import { isDev } from './utils/index.ts'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // 每次查询都会尝试请求最新数据，但会先用旧缓存兜底, 避免白屏, 可以单独设置
+      // 数据在多久内被视为新鲜的, 单位毫秒, 默认为0
+      staleTime: 0, 
+      // 缓存数据在内存中保留的时间, 默认为5分钟, 不能单独设置
+      gcTime: 5 * 60 * 1000
+    }
+  }
+})
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
